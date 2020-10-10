c4998596eeebe25259d6f7e8c0508a29c43e5223
Dip Roy <69411261+ldstr@users.noreply.github.com>
2020-10-10 17:15:21 +1000

    Update index.js

--- index.js
+++ index.js
@@ -1,28 +1,30 @@
-onload = () => {
-  setInterval(() => {
-    chrome.storage.local.get(['initialized', 'test_mode'], ({ initialized, test_mode }) => {
-      // TODO: Switch this to something much more efficient.
-      if (initialized && test_mode && (screen.availHeight || screen.height - 30) > window.innerHeight) {
-        chrome.runtime.sendMessage({
-          socket: true,
-          type: 'fullscreen-exit'
-        });
+if (!location.href.startsWith('chrome')) {
+  onload = () => {
+    setInterval(() => {
+      chrome.storage.local.get(['initialized', 'test_mode'], ({ initialized, test_mode }) => {
+        // TODO: Switch this to something much more efficient.
+        if (initialized && test_mode && (screen.availHeight || screen.height - 30) > window.innerHeight) {
+          chrome.runtime.sendMessage({
+            socket: true,
+            type: 'fullscreen-exit'
+          });
 
-        alert('Test mode is enabled, please go to full screen mode!');
-      }
-    });
-  }, 1000);
-};
+          alert('Test mode is enabled, please go to full screen mode!');
+        }
+      });
+    }, 1000);
+  };
 
-chrome.runtime.onMessage.addListener((request) => {
-  if (request.type) {
-    switch (request.type) {
-      case 'private-message': {
-        alert(`You received a private message from your teacher!\n\nMessage: ${request.message}`);
-        break;
+  chrome.runtime.onMessage.addListener((request) => {
+    if (request.type) {
+      switch (request.type) {
+        case 'private-message': {
+          alert(`You received a private message from your teacher!\n\nMessage: ${request.message}`);
+          break;
+        }
       }
     }
-  }
 
-  return true;
-});
+    return true;
+  });
+}
